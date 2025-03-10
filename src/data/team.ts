3b5f947cc76c75d6ca41ea9a0fc93d899e6b54f4
Kushwant KR <143092401+mdecoder24@users.noreply.github.com>
2025-03-11 04:02:49 +0530

    Updated team names

--- src/data/team.ts
+++ src/data/team.ts
@@ -3,43 +3,43 @@ import { MemberProps } from "./types";
 // data.ts
 export const team : MemberProps[] = [
     {
-      name: "Sarah Johnson",
-      title: "Event Coordinator",
+      name: "Shreeya Pawar",
+      title: "Organiser",
       image: "https://public.readdy.ai/ai/img_res/a8867ef2bf10e4fc157a5a23997ce69d.jpg",
     },
     {
-      name: "David Zhang",
-      title: "Technical Director",
+      name: "Mohd Raza",
+      title: "Co-Organiser",
       image: "https://public.readdy.ai/ai/img_res/e626815941e82ce283ce9a6e9fb0c9ab.jpg",
     },
     {
-      name: "Priya Patel",
-      title: "Marketing Lead",
+      name: "Aditya Gupta",
+      title: "Curator",
       image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
     },
     {
-      name: "Carlos Rodriguez",
-      title: "Speaker Relations",
+      name: "Molishaa",
+      title: "Executive Producer",
       image: "https://public.readdy.ai/ai/img_res/081f4d843a67dc5ce657eed05d133d73.jpg",
     },
     {
-      name: "Sarah Johnson",
-      title: "Event Coordinator",
+      name: "Vinay",
+      title: "Event Manager",
       image: "https://public.readdy.ai/ai/img_res/a8867ef2bf10e4fc157a5a23997ce69d.jpg",
     },
     {
-      name: "David Zhang",
-      title: "Technical Director",
+      name: "Vishruth",
+      title: "Sponsorship and Budget Manager",
       image: "https://public.readdy.ai/ai/img_res/e626815941e82ce283ce9a6e9fb0c9ab.jpg",
     },
     {
-      name: "Priya Patel",
-      title: "Marketing Lead",
+      name: "Durga",
+      title: "Video and Production Lead",
       image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
     },
     {
-      name: "Carlos Rodriguez",
-      title: "Speaker Relations",
+      name: "Akhil",
+      title: "Video and Production Lead",
       image: "https://public.readdy.ai/ai/img_res/081f4d843a67dc5ce657eed05d133d73.jpg",
     },
   ];
