d8ed240a33b595013a30a49ca1314d8f0ffb00c8
TEDx MLRIT <tedxmlrit@mlrit.ac.in>
2025-03-11 00:18:16 +0000

    teams

---
+++ src/data/team.ts
@@ -0,0 +1,129 @@
+import { MemberProps } from "./types";
+
+// data.ts
+export const team : MemberProps[] = [
+    {
+      name: "Shreeya Pawar",
+      title: "Organiser",
+      image: "/public/TeamPics/Shreeya.JPG",
+    },
+    {
+      name: "Mohd Raza",
+      title: "Co-Organiser",
+      image: "/public/TeamPics/Raza.JPG",
+    },
+    {
+      name: "Aditya Gupta",
+      title: "Curator",
+      image: "/public/TeamPics/Aditya.JPG",
+    },
+    {
+      name: "Molishaa",
+      title: "Executive Producer",
+      image: "./public/TeamPics/Molishaa.JPG",
+    },
+    {
+      name: "Vinay",
+      title: "Event Manager",
+      image: "public/TeamPics/Vinay.JPG",
+    },
+    {
+      name: "Vishruth",
+      title: "Sponsorship and Budget Manager",
+      image: "https://public.readdy.ai/ai/img_res/e626815941e82ce283ce9a6e9fb0c9ab.jpg",
+    },
+    {
+      name: "Durga",
+      title: "Video and Production Lead",
+      image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
+    },
+    {
+      name: "Akhil",
+      title: "Video and Production Lead",
+      image: "https://public.readdy.ai/ai/img_res/081f4d843a67dc5ce657eed05d133d73.jpg",
+    },
+    {
+      name: "Kushwant KR",
+      title: "Website Manager",
+      image: "public/TeamPics/Kush.JPG",
+    },
+    {
+      name: "Mohd Raza",
+      title: "Co-Organiser",
+      image: "https://public.readdy.ai/ai/img_res/e626815941e82ce283ce9a6e9fb0c9ab.jpg",
+    },
+    {
+      name: "Aditya Gupta",
+      title: "Curator",
+      image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
+    },
+    {
+      name: "Molishaa",
+      title: "Executive Producer",
+      image: "https://public.readdy.ai/ai/img_res/081f4d843a67dc5ce657eed05d133d73.jpg",
+    },
+    {
+      name: "Vinay",
+      title: "Event Manager",
+      image: "https://public.readdy.ai/ai/img_res/a8867ef2bf10e4fc157a5a23997ce69d.jpg",
+    },
+    {
+      name: "Vishruth",
+      title: "Sponsorship and Budget Manager",
+      image: "https://public.readdy.ai/ai/img_res/e626815941e82ce283ce9a6e9fb0c9ab.jpg",
+    },
+    {
+      name: "Durga",
+      title: "Video and Production Lead",
+      image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
+    },
+    {
+      name: "Akhil",
+      title: "Video and Production Lead",
+      image: "https://public.readdy.ai/ai/img_res/081f4d843a67dc5ce657eed05d133d73.jpg",
+    },
+  ];
+
+  export const hometeam : MemberProps[] = [
+    {
+      name: "Shreeya Pawar",
+      title: "Organiser",
+      image: "public/TeamPics/Shreeya.JPG",
+    },
+    {
+      name: "Mohd Raza",
+      title: "Co-Organiser",
+      image: "public/TeamPics/Raza.JPG",
+    },
+    {
+      name: "Aditya Gupta",
+      title: "Curator",
+      image: "public/TeamPics/Aditya.JPG",
+    },
+    {
+      name: "Molishaa",
+      title: "Executive Producer",
+      image: "public/TeamPics/Molishaa.JPG",
+    },
+    {
+      name: "Vinay",
+      title: "Event Manager",
+      image: "public/TeamPics/Vinay.JPG",
+    },
+    {
+      name: "Vishruth",
+      title: "Sponsorship and Budget Manager",
+      image: "https://public.readdy.ai/ai/img_res/e626815941e82ce283ce9a6e9fb0c9ab.jpg",
+    },
+    {
+      name: "Durga",
+      title: "Video and Production Lead",
+      image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
+    },
+    {
+      name: "Akhil",
+      title: "Video and Production Lead",
+      image: "https://public.readdy.ai/ai/img_res/081f4d843a67dc5ce657eed05d133d73.jpg",
+    },
+  ];
+  
