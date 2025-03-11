113d9d664bb2075e14512171e072619444c8ceb5
TEDx MLRIT <tedxmlrit@mlrit.ac.in>
2025-03-11 15:37:06 +0530

    team data

--- src/data/team.ts
+++ src/data/team.ts
@@ -35,7 +35,7 @@ export const team : MemberProps[] = [
     {
       name: "Durga",
       title: "Video and Production Lead",
-      image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
+      image: "public/TeamPics/durga.JPG",
     },
     {
       name: "Akhil",
@@ -48,40 +48,246 @@ export const team : MemberProps[] = [
       image: "/TeamPics/Kush.JPG",
     },
     {
-      name: "Mohd Raza",
-      title: "Co-Organiser",
-      image: "https://public.readdy.ai/ai/img_res/e626815941e82ce283ce9a6e9fb0c9ab.jpg",
+      name: "Shashank",
+      title: "Designer",
+      image: "/public/TeamPics/shashank.JPG",
     },
     {
-      name: "Aditya Gupta",
-      title: "Curator",
-      image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
+      name: "Haneesh",
+      title: "Communications, Editorial & Marketing",
+      image: "/public/TeamPics/Haneesh.JPG",
     },
     {
-      name: "Molishaa",
-      title: "Executive Producer",
-      image: "https://public.readdy.ai/ai/img_res/081f4d843a67dc5ce657eed05d133d73.jpg",
+      name: "Praneeth Pothnak",
+      title: "Sponsorship & Budget Manager",
+      image: "/public/TeamPics/praneeth.JPG",
     },
     {
-      name: "Vinay",
+      name: "Harish Badhey",
       title: "Event Manager",
-      image: "https://public.readdy.ai/ai/img_res/a8867ef2bf10e4fc157a5a23997ce69d.jpg",
+      image: "/public/TeamPics/Harish.JPG",
     },
     {
-      name: "Vishruth",
-      title: "Sponsorship and Budget Manager",
-      image: "https://public.readdy.ai/ai/img_res/e626815941e82ce283ce9a6e9fb0c9ab.jpg",
+      name: "Muzzammil",
+      title: "Video & Production Lead",
+      image: "/public/TeamPics/muzzan.JPG",
     },
     {
-      name: "Durga",
-      title: "Video and Production Lead",
-      image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
+      name: "Aryan Sreedhar",
+      title: "Curator",
+      image: "/public/TeamPics/Aryan 3.JPG",
     },
     {
-      name: "Akhil",
-      title: "Video and Production Lead",
-      image: "https://public.readdy.ai/ai/img_res/081f4d843a67dc5ce657eed05d133d73.jpg",
+      name: "Siddharth",
+      title: "Executive Producer",
+      image: "/public/TeamPics/Siddharth.JPG",
     },
+    {
+      name: "Akhila",
+      title: "Wedsite Manager",
+      image: "/public/TeamPics/Akhila.JPG",
+    },
+    {
+      name: "Amisha",
+      title: "Designer",
+      image: "/public/TeamPics/Amisha.JPG",
+    },
+    {
+      name: "Lasya",
+      title: "Communications, Editorial & Marketing",
+      image: "/public/TeamPics/lasya.JPG",
+    },
+    {
+      name: "Venkat",
+      title: "Video & Production Lead",
+      image: "/",
+    },
+    {
+      name: "Rohan",
+      title: "Sponsorship & Budget Manager",
+      image: "/public/TeamPics/Rohan.JPG",
+    },
+    {
+      name: "Rahaman",
+      title: "Event Manager",
+      image: "/public/TeamPics/rahaman.JPG",
+    },
+    {
+      name: "Charishma",
+      title: "Executive Producer",
+      image: "/public/TeamPics/charishma.JPG",
+    },
+    {
+      name: "Moizz",
+      title: "Curator",
+      image: "/public/TeamPics/moiz.JPG",
+    },
+    {
+      name: "Pavani",
+      title: "Designer",
+      image: "/public/TeamPics/pavani.JPG",
+    },
+    {
+      name: "Shreya",
+      title: "Communications, Editorial & Marketing",
+      image: "/public/TeamPics/shreya.JPG",
+    },
+    {
+      name: "Sagar",
+      title: "Video & Production Lead",
+      image: "/public/TeamPics/sagar.JPG",
+    },
+    {
+      name: "Nitisha",
+      title: "Event Manager",
+      image: "/public/TeamPics/nitisha.JPG",
+    },
+    {
+      name: "Neeharika",
+      title: "Executive Producer",
+      image: "/public/TeamPics/niharika.JPG",
+    },
+    {
+      name: "Rishitha",
+      title: "Communications, Editorial & Marketing",
+      image: "/public/TeamPics/Rishitha.JPG",
+    },
+    {
+      name: "Keerthana",
+      title: "Curator",
+      image: "/public/TeamPics/keerthana.JPG",
+    },
+
+    {
+      name: "Arnav",
+      title: "Volunteer",
+      image: "/public/TeamPics/Arnav.JPG",
+    },
+    {
+      name: "Adit",
+      title: "Volunteer",
+      image: "/public/TeamPics/Adith.JPG",
+    },
+    {
+      name: "Ashritha",
+      title: "Volunteer",
+      image: "/public/TeamPics/ashritha.JPG",
+    },
+    {
+      name: "Srikanth",
+      title: "Volunteer",
+      image: "/public/TeamPics/Srikanth.JPG",
+    },
+    {
+      name: "Shankan",
+      title: "Volunteer",
+      image: "/public/TeamPics/shankan.JPG",
+    },
+    {
+      name: "Mahitha",
+      title: "Volunteer",
+      image: "/public/TeamPics/mahitha.JPG",
+    },
+    {
+      name: "Rishika",
+      title: "Volunteer",
+      image: "/public/TeamPics/Rishika.JPG",
+    },
+    {
+      name: "Harsh",
+      title: "Volunteer",
+      image: "/public/TeamPics/Harsh.JPG",
+    },
+    {
+      name: "Aryan",
+      title: "Volunteer",
+      image: "/public/TeamPics/Aryan.JPG",
+    },{
+      name: "Sunayana",
+      title: "Volunteer",
+      image: "/public/TeamPics/suni.JPG",
+    },
+    {
+      name: "Akshita",
+      title: "Volunteer",
+      image: "/public/TeamPics/Akshitha.JPG",
+    },
+    {
+      name: "Faiza",
+      title: "Volunteer",
+      image: "/public/TeamPics/Faiza.JPG",
+    },
+    {
+      name: "Snigdha",
+      title: "Volunteer",
+      image: "/public/TeamPics/snigdha.JPG",
+    },
+    {
+      name: "Pooja",
+      title: "Volunteer",
+      image: "/public/TeamPics/pooja.JPG",
+    },
+    {
+      name: "Sujay",
+      title: "Volunteer",
+      image: "/public/TeamPics/Sujay.JPG",
+    },
+    {
+      name: "Koushik",
+      title: "Volunteer",
+      image: "/public/TeamPics/koushik.JPG",
+    },
+    {
+      name: "Sagam Koushik",
+      title: "Volunteer",
+      image: "/public/TeamPics/saggam.JPG",
+    },
+    {
+      name: "Omaansh",
+      title: "Volunteer",
+      image: "/public/TeamPics/omaansh.JPG",
+    },
+    {
+      name: "Sruthi",
+      title: "Volunteer",
+      image: "/public/TeamPics/Shruthi.JPG",
+    },
+    {
+      name: "Sneha",
+      title: "Volunteer",
+      image: "/public/TeamPics/Sneha.JPG",
+    },
+    {
+      name: "Zubiyah",
+      title: "Volunteer",
+      image: "/public/TeamPics/Zubiya.JPG",
+    },
+    {
+      name: "Varshini",
+      title: "Volunteer",
+      image: "/public/TeamPics/Varshini.JPG",
+    },
+    {
+      name: "Nishi",
+      title: "Volunteer",
+      image: "/public/TeamPics/Nishi.JPG",
+    },
+    {
+      name: "Richard",
+      title: "Volunteer",
+      image: "/public/TeamPics/Richard.JPG",
+    },
+    {
+      name: "Swetha",
+      title: "Volunteer",
+      image: "/public/TeamPics/Swetha.JPG",
+    },
+    {
+      name: "Vineela",
+      title: "Volunteer",
+      image: "/public/TeamPics/vineela.JPG",
+    },
+    
   ];
 
   export const hometeam : MemberProps[] = [
@@ -118,7 +324,7 @@ export const team : MemberProps[] = [
     {
       name: "Durga",
       title: "Video and Production Lead",
-      image: "https://public.readdy.ai/ai/img_res/84f999e1505dd383566d14c0315f9c0b.jpg",
+      image: "/public/TeamPics/durga.JPG",
     },
     {
       name: "Akhil",
