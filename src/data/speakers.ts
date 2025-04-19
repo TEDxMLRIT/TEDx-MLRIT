f6a3d844934a7aa8d9ba6c338ce0a401a06d8edc
TEDx MLRIT <tedxmlrit@mlrit.ac.in>
2025-04-19 22:15:42 +0530

    Update speakers.ts

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -3,10 +3,11 @@ import { Speaker } from "./types";
 
 export const speakers: Speaker[] = [
   {
-    name: "XXX",
-    title: "XXX",
-    description: "XXX.",
-    image: "/Sponsors/uuu.svg"
+    name: "Kavya Kalyanram",
+    title: "Lawyer,Actor and Entrepreneur",
+    description: "“A woman of many talents and one inspiring journey.”  
+Kavya Kalyanram is a powerhouse redefining versatility.",
+    image: "/Sponsors/kavya.jpg"
   },
   {
     name: "XXX",
