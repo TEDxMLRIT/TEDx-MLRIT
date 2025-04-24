80a9164afece9d8606b7f11081d47feca3081195
TEDx MLRIT <tedxmlrit@mlrit.ac.in>
2025-04-24 12:23:53 +0530

    Update blogs.ts

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -7,14 +7,14 @@ export const blogPosts = [
       category: "Technology",
       date: "March 1, 2025",
       image: "https://public.readdy.ai/ai/img_res/0b8c060827e33037135dae1ae71a7522.jpg",
-      url:"https://moizwrite.medium.com/lost-in-the-rush-are-your-values-your-compass-b9005e481b22"
+      url:"https://medium.com/@tedxmlrit/the-echoes-of-tomorrow-because-ideas-dont-die-they-resonate-7e72757ce691"
     },
     {
       title: "If Motivation Works, Why Are You Still Stuck?",
       category: "Innovation",
       date: "March 2, 2025",
       image: "https://public.readdy.ai/ai/img_res/98d764504f1dac203aa2784954e1e3be.jpg",
-      url:"https://moizwrite.medium.com/lost-in-the-rush-are-your-values-your-compass-b9005e481b22"
+      url:"https://medium.com/@tedxmlrit/if-motivation-works-why-are-you-still-stuck-6c23afa49f56"
     },
     {
       title: "Lost in the Rush: Are Your Values Your Compass?",
