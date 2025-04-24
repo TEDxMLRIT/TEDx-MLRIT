ee29f064d2b269c63dc98d00130c77c6c393f790
TEDx MLRIT <tedxmlrit@mlrit.ac.in>
2025-04-24 13:16:49 +0530

    Update blogs.ts

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -4,22 +4,22 @@
 export const blogPosts = [
     {
       title: "The Echoes of Tomorrow: Because Ideas Don't Die—They Resonate",
-      category: "Technology",
+      category: "Inspiration",
       date: "March 1, 2025",
       image: "https://public.readdy.ai/ai/img_res/0b8c060827e33037135dae1ae71a7522.jpg",
       url:"https://medium.com/@tedxmlrit/the-echoes-of-tomorrow-because-ideas-dont-die-they-resonate-7e72757ce691"
     },
     {
       title: "If Motivation Works, Why Are You Still Stuck?",
-      category: "Innovation",
+      category: "Reality Check",
       date: "March 2, 2025",
       image: "https://public.readdy.ai/ai/img_res/98d764504f1dac203aa2784954e1e3be.jpg",
       url:"https://medium.com/@tedxmlrit/if-motivation-works-why-are-you-still-stuck-6c23afa49f56"
     },
     {
       title: "Lost in the Rush: Are Your Values Your Compass?",
-      category: "Science",
-      date: "March 3, 2025",
+      category: "philosophy",
+      date: "March 26, 2024",
       image: "https://public.readdy.ai/ai/img_res/bc976f6af2c706d9fa8b4826a95b2e5e.jpg",
       url:"https://moizwrite.medium.com/lost-in-the-rush-are-your-values-your-compass-b9005e481b22"
     },
